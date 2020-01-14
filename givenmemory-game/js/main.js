9a9716afb1737be5676d65f4d3a203060e26f8ae
Denise Cheung <denise@denises-mbp.mynet>
2020-01-14 12:42:34 +0000

    created game logic with arrays

--- givenmemory-game/js/main.js
+++ givenmemory-game/js/main.js
@@ -1,7 +1,23 @@
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
 
-console.log("User flipped " + cardOne);
-console.log ("User flipped " + cardThree);
+const cards = ["queen", "queen", "king", "king"]; 
+
+const cardsInPlay = [ ];
+
+let cardOne = "queen";
+cardsInPlay.push ("cardOne");
+console.log("User flipped queen"); 
+
+let cardTwo = cards[2]; 
+cardsInPlay.push =cards[2];
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2) {
+cardsInPlay[0] === cardsInPlay[1] 
+alert("You found a match!")
+} 
+
+else {
+alert ("Sorry, try again.")
+}
+
+
