7532a68b75f3174ad7f0ad2176977a9c754a7fd0
Denise Cheung <denise@denises-mbp.mynet>
2020-01-16 11:59:59 +0000

    created game logic with functions

--- givenmemory-game/js/main.js
+++ givenmemory-game/js/main.js
@@ -1,23 +1,28 @@
 
-const cards = ["queen", "queen", "king", "king"]; 
+const cards = ["queen", "queen", "king", "king"]; //created an array cards within 4 elements inside
 
-const cardsInPlay = [ ];
+const cardsInPlay = [ ]; //cards an array with no elements inside. purpose?
 
-let cardOne = "queen";
-cardsInPlay.push ("cardOne");
-console.log("User flipped queen"); 
-
-let cardTwo = cards[2]; 
-cardsInPlay.push =cards[2];
-console.log("User flipped king");
-
-if (cardsInPlay.length === 2) {
-cardsInPlay[0] === cardsInPlay[1] 
-alert("You found a match!")
-} 
-
-else {
-alert ("Sorry, try again.")
+function checkForMatch() { //created a function named checkForMatch
+	if (cardsInPlay[0] === cardsInPlay[1]) { //if they equal then display this is a match etc. but I have nothing in cardsInPlay array?
+	  console.log("You found a match!");
+	} else {
+	  console.log("Sorry, try again.");
+	}
 }
 
+
+function flipCard(cardId) { //created a function named flipCard with the parameter cardId inside
+	cardsInPlay.push(cards[cardId]); //this will add whatever argument is given for cardId to the array cardsInPlay
+	console.log("User flipped " +cards[cardId]); //then it will log users flipped (argument given for cardID)
+
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+
+	}
+
+}
+
+flipCard(0); //giving the parameter inside flipCard, i.e: cardId the value 0
+flipCard(2);// giving the parameter inside flipCard the value 2
 
