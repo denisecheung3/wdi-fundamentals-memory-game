abb36b2b9d3fce0c0ac114e461162987c8e31479
Denise Cheung <denise@denises-mbp.mynet>
2020-01-16 12:13:04 +0000

    created game logic with objects

--- givenmemory-game/js/main.js
+++ givenmemory-game/js/main.js
@@ -1,20 +1,46 @@
 
-const cards = ["queen", "queen", "king", "king"]; //created an array cards within 4 elements inside
+const cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+
+	},
+	{
+		rank:"king",
+		suit:"diamongs",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
 
 const cardsInPlay = [ ]; //cards an array with no elements inside. purpose?
 
 function checkForMatch() { //created a function named checkForMatch
 	if (cardsInPlay[0] === cardsInPlay[1]) { //if they equal then display this is a match etc. but I have nothing in cardsInPlay array?
-	  console.log("You found a match!");
+	  alert("You found a match!");
 	} else {
-	  console.log("Sorry, try again.");
+	  alert("Sorry, try again.");
 	}
 }
 
 
 function flipCard(cardId) { //created a function named flipCard with the parameter cardId inside
-	cardsInPlay.push(cards[cardId]); //this will add whatever argument is given for cardId to the array cardsInPlay
-	console.log("User flipped " +cards[cardId]); //then it will log users flipped (argument given for cardID)
+	cardsInPlay.push(cards[cardId].rank); //this will add whatever argument is given for cardId to the array cardsInPlay
+	console.log("User flipped " +cards[cardId].rank); //then it will log users flipped (argument given for cardID)
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
